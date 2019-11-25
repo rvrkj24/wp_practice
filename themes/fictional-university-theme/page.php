4fa3f869037e7b9f644db5ab755e474be2bb8c08
Rajeev Jaiswal <rjaiswal@AD.MSYSTECHNOLOGIES.COM>
2019-11-25 15:39:48 +0530

    Just synching

--- themes/fictional-university-theme/page.php
+++ themes/fictional-university-theme/page.php
@@ -3,7 +3,7 @@
 get_header();
 
 while(have_posts()) {
-    the_post(); 
+    the_post();
     ?>
      <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>);"></div>
@@ -12,36 +12,36 @@ while(have_posts()) {
       <div class="page-banner__intro">
         <p>DONT FORGET TO UPDATE ME LATER</p>
       </div>
-    </div>  
+    </div>
   </div>
 
   <div class="container container--narrow page-section">
 
-<?php 
+<?php
 
-  $theParent = wp_get_post_parent_id(get_the_ID());
+$theParent = wp_get_post_parent_id(get_the_ID());
 
   if ($theParent) { ?>
   <div class="metabox metabox--position-up metabox--with-home-link">
       <p>
-      <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> 
+      <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a>
       <span class="metabox__main"><?php the_title(); ?></span>
       </p>
     </div>
-  
-    <!-- 
+
+    <!--
     the_title() = will output the current title of page or post.
-    get_the_title() = will allow to pass the id number in the parenthesis, 
+    get_the_title() = will allow to pass the id number in the parenthesis,
     and will give you the title for that id instead of just post that you are currently loop through
      -->
 
   <?php }
-  
+
   ?>
 
 
-    
-    <?php 
+
+    <?php
 
     $testArray = get_pages(array(
       'child_of' => get_the_ID()
@@ -65,7 +65,7 @@ while(have_posts()) {
               'sort_column' => 'menu_order'
             ));
         ?>
-       
+
       </ul>
     </div>
     <?php } ?>
@@ -75,8 +75,8 @@ while(have_posts()) {
     </div>
 
   </div>
-    
-<?php 
+
+<?php
 }
 
 get_footer();
