b88d5da181364316e5f7110ceb1999e8df7a066d
rvrkj24 <rjaiswal@msystechnologies.com>
2019-12-02 09:35:32 +0530

    Adding more in custom post types

--- themes/fictional-university-theme/front-page.php
+++ themes/fictional-university-theme/front-page.php
@@ -14,29 +14,43 @@
     <div class="full-width-split__one">
       <div class="full-width-split__inner">
         <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
+        <?php
 
-        <div class="event-summary">
+          $homepageEvents = new WP_Query(array(
+            'posts_per_page' => 2,
+            'post_type' => 'event'
+          ));
+
+          while($homepageEvents -> have_posts()) {
+            $homepageEvents->the_post();
+             ?>
+            
+          <div class="event-summary">
           <a class="event-summary__date t-center" href="#">
-            <span class="event-summary__month">Mar</span>
-            <span class="event-summary__day">25</span>
+            <span class="event-summary__month"><?php the_time('M'); ?></span>
+            <span class="event-summary__day"><?php the_time('d') ?></span>
           </a>
           <div class="event-summary__content">
-            <h5 class="event-summary__title headline headline--tiny"><a href="#">Poetry in the 100</a></h5>
-            <p>Bring poems you&rsquo;ve wrote to the 100 building this Tuesday for an open mic and snacks. <a href="#" class="nu gray">Learn more</a></p>
+            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+            <p><?php if(has_excerpt()){
+              echo get_the_excerpt();
+            } else
+             {
+               echo wp_trim_words(get_the_content(), 18);
+             }
+              ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
           </div>
         </div>
-        <div class="event-summary">
-          <a class="event-summary__date t-center" href="#">
-            <span class="event-summary__month">Apr</span>
-            <span class="event-summary__day">02</span>
-          </a>
-          <div class="event-summary__content">
-            <h5 class="event-summary__title headline headline--tiny"><a href="#">Quad Picnic Party</a></h5>
-            <p>Live music, a taco truck and more can found in our third annual quad picnic day. <a href="#" class="nu gray">Learn more</a></p>
-          </div>
-        </div>
+          
+          <?php }
+          ?>
 
-        <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
+
+
+        
+       
+        
+        <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>" class="btn btn--blue">View All Events</a></p>
 
       </div>
     </div>
@@ -61,7 +75,13 @@
              <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
             </h5>
             <p>
-            <?php echo wp_trim_words(get_the_content(), 18); ?>
+            <?php if(has_excerpt()){
+              echo get_the_excerpt();
+            } else
+             {
+               echo wp_trim_words(get_the_content(), 18);
+             }
+              ?>
             <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a>
             </p>
           </div>
