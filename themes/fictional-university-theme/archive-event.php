d2a1d1c8fed9e5780221c88c10e5e403fe4e391c
rvrkj24 <rjaiswal@msystechnologies.com>
2019-12-13 09:26:55 +0530

    Removed Asbab Added custom filed plugin

--- themes/fictional-university-theme/archive-event.php
+++ themes/fictional-university-theme/archive-event.php
@@ -13,12 +13,20 @@ get_header(); ?>
 
  <div class="container container--narrow page-section">
   <?php
+  
     while(have_posts()) {
       the_post(); ?>
         <div class="event-summary">
-          <a class="event-summary__date t-center" href="#">
-            <span class="event-summary__month"><?php the_time('M'); ?></span>
-            <span class="event-summary__day"><?php the_time('d') ?></span>
+        <a class="event-summary__date t-center" href="#">
+            <span class="event-summary__month">
+              <?php 
+              $eventDate = new DateTime(get_field('event_date'));
+              echo $eventDate->format('M')
+              ?>
+              </span>
+              <span class="event-summary__day">
+              <?php echo $eventDate->format('d') ?>
+            </span>
           </a>
           <div class="event-summary__content">
             <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
@@ -28,8 +36,10 @@ get_header(); ?>
     <?php }
 
   echo paginate_links();
-
   ?>
+<hr class="section-break">
+<p>Looking for a recap of past events? <a href="<?php echo site_url('/past-events'); ?>">Check out our past events archive</a>.</p>
+
  </div>
 
 <?php
--- themes/fictional-university-theme/front-page.php
+++ themes/fictional-university-theme/front-page.php
@@ -15,42 +15,60 @@
       <div class="full-width-split__inner">
         <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
         <?php
-
+          $today = date('Ymd');
           $homepageEvents = new WP_Query(array(
-            'posts_per_page' => 2,
-            'post_type' => 'event'
+            'posts_per_page' => 2, 
+            'post_type' => 'event',
+            'meta_key' => 'event_date',
+            'orderby' => 'meta_value_num',
+            'order' => 'ASC',
+            'meta_query' => array(
+              array(
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numeric'
+              )
+            )
           ));
 
           while($homepageEvents -> have_posts()) {
             $homepageEvents->the_post();
-             ?>
-            
-          <div class="event-summary">
+        ?>
+            <div class="event-summary">
           <a class="event-summary__date t-center" href="#">
-            <span class="event-summary__month"><?php the_time('M'); ?></span>
-            <span class="event-summary__day"><?php the_time('d') ?></span>
+            <span class="event-summary__month">
+              <?php 
+              $eventDate = new DateTime(get_field('event_date'));
+              echo $eventDate->format('M')
+              ?>
+              </span>
+              <span class="event-summary__day">
+              <?php echo $eventDate->format('d') ?>
+            </span>
           </a>
           <div class="event-summary__content">
-            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-            <p><?php if(has_excerpt()){
+            <h5 class="event-summary__title headline headline--tiny">
+              <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            </h5>
+            <p>
+              <?php if(has_excerpt()){
               echo get_the_excerpt();
             } else
              {
                echo wp_trim_words(get_the_content(), 18);
              }
-              ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+              ?> 
+              <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a>
+            </p>
           </div>
         </div>
-          
           <?php }
           ?>
-
-
-
-        
-       
-        
-        <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>" class="btn btn--blue">View All Events</a></p>
+             
+        <p class="t-center no-margin">
+          <a href="<?php echo get_post_type_archive_link('event'); ?>" class="btn btn--blue">View All Events</a>
+        </p>
 
       </div>
     </div>
