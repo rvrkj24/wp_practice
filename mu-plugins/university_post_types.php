8449c58998beea691bbfcda9580b8da8fa5b9a8b
rvrkj24 <rjaiswal@msystechnologies.com>
2019-12-23 09:09:40 +0530

    Adding Event and program Post type

--- mu-plugins/university_post_types.php
+++ mu-plugins/university_post_types.php
@@ -1,6 +1,7 @@
 <?php 
 
 function university_post_types() {
+    // Event Post type
     register_post_type('event', array(
         'supports' => array('title', 'editor','excerpt'),
         'rewrite' => array('slug' => 'events'),
@@ -14,6 +15,36 @@ function university_post_types() {
             'singular_name' => 'Event'
         ),
         'menu_icon' => 'dashicons-calendar'
+    ));
+
+     // Program Post type
+     register_post_type('program', array(
+        'supports' => array('title', 'editor'),
+        'rewrite' => array('slug' => 'programs'),
+        'has_archive' => true,
+        'public' => true,
+        'labels' => array(
+            'name' => 'Programs',
+            'add_new_item' => 'Add New Program',
+            'edit_item' => 'Edit Program',
+            'all_items' => 'All Programs',
+            'singular_name' => 'Program'
+        ),
+        'menu_icon' => 'dashicons-awards'
+    ));
+
+    // Professor Post type
+    register_post_type('professor', array(
+        'supports' => array('title', 'editor'),
+        'public' => true,
+        'labels' => array(
+            'name' => 'Professors',
+            'add_new_item' => 'Add New Professor',
+            'edit_item' => 'Edit Professor',
+            'all_items' => 'All Professors',
+            'singular_name' => 'Professor'
+        ),
+        'menu_icon' => 'dashicons-welcome-learn-more'
     ));
 }
 
--- themes/fictional-university-theme/single-event.php
+++ themes/fictional-university-theme/single-event.php
@@ -25,6 +25,26 @@ while(have_posts()) {
 
   <div class="generic-content">
   <?php the_content(); ?>
+
+  <?php
+  
+  $relatedPrograms = get_field('related_programs');
+  
+  //print_r($relatedPrograms); -to know the return type of variable $relatedPrograms
+  if($relatedPrograms){
+    echo'<hr class="section-break">';
+    echo'<h2 class="headline headline--medium">Related Program(s)</h2>';
+    echo'<ul class="link-list min-list">';
+  foreach($relatedPrograms as $program) { ?>
+    
+    <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
+    
+  <?php } 
+    echo'</ul>'  ;
+  }
+  
+  ?>
+
   </div>
   
   </div>
